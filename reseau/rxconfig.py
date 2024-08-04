7f46dda548904c13ada9e8b30b92850ee0d5d714
baudet_c <baudet_c@etna-alternance.net>
2024-08-04 10:34:50 +0200

    fix: ENV VAR for prod deployment

--- reseau/rxconfig.py
+++ reseau/rxconfig.py
@@ -3,15 +3,20 @@ import reflex as rx
 
 
 LOCAL_SQLITE_URL = "sqlite:///reseau.db"
-AMAZON_RDS_POSTGRESQL_URL = "postgresql://postgres:5zjbhMflkRoTRhbEtKO3@reseau-db-instance.chsg2oummucl.eu-west-3.rds.amazonaws.com/reseau"  # noqa
-# AMAZON_RDS_POSTGRESQL_URL = os.environ.get("AMAZON_RDS_POSTGRESQL_URL")
+AMAZON_RDS_POSTGRESQL_URL = os.environ.get(
+    "AMAZON_RDS_POSTGRESQL_URL",
+    "postgresql://postgres:5zjbhMflkRoTRhbEtKO3@reseau-db-instance.chsg2oummucl.eu-west-3.rds.amazonaws.com/reseau"  # noqa
+)
 
 APP_IP = os.environ.get("APP_IP", "0.0.0.0")
 BACK_PORT = os.environ.get("BACK_PORT", "8000")
-GMAIL_APP_PASSWORD = os.environ.get("GMAIL_APP_PASSWORD")
+GMAIL_APP_PASSWORD = os.environ.get(
+    "GMAIL_APP_PASSWORD",
+    "xpic zpwf rvxx jqpt"
+)
 
 config = rx.Config(
     app_name="reseau",
     api_url=f"http://{APP_IP}:{BACK_PORT}",
-    db_url=LOCAL_SQLITE_URL,
+    db_url=AMAZON_RDS_POSTGRESQL_URL,
 )
