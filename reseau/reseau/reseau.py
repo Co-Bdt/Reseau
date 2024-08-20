aeef1b80bef6022ec19d33ee051c84511ee3d458
baudet_c <baudet_c@etna-alternance.net>
2024-08-20 08:54:08 +0200

    fix: change double quotes to single on reseau App file [skip ci]

--- reseau/reseau/reseau.py
+++ reseau/reseau/reseau.py
@@ -3,27 +3,27 @@ import reflex as rx
 from .common.style import style
 
 
-HOME_ROUTE = "/"
-REGISTER_ROUTE = "/rejoindre"
-LOGIN_ROUTE = "/connexion"
-PROFILE_ROUTE = "/profil"
-MEMBERS_ROUTE = "/membres"
+HOME_ROUTE = '/'
+REGISTER_ROUTE = '/rejoindre'
+LOGIN_ROUTE = '/connexion'
+PROFILE_ROUTE = '/profil'
+MEMBERS_ROUTE = '/membres'
 
 
 google_ads_script1 = rx.script(
     src=(
-        "https://www.googletagmanager.com/gtag/js?"
-        "id=AW-16676736858"
+        'https://www.googletagmanager.com/gtag/js?'
+        'id=AW-16676736858'
     ),
 )
 
 google_ads_script2 = rx.script(
-    """
+    '''
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
     gtag('config', 'AW-16676736858');
-    """,
+    ''',
 )
 
 
@@ -33,11 +33,11 @@ app = rx.App(
         google_ads_script2
     ],
     theme=rx.theme(
-        appearance="light",
-        accent_color="amber",
+        appearance='light',
+        accent_color='amber',
     ),
     style=style,  # Global style configuration
     stylesheets=[  # Specific stylesheets
-        "/styles.css",  # This path is relative to assets/
+        '/styles.css',  # This path is relative to assets/
     ],
 )
