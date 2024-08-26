8e654ff9bb624189483c49990703548602646cae
baudet_c <baudet_c@etna-alternance.net>
2024-08-26 11:14:25 +0200

    fix: search can be done for users, can access discussion from the user's name and remove displayed email from members page

--- reseau/reseau/components/members_grid.py
+++ reseau/reseau/components/members_grid.py
@@ -10,41 +10,97 @@ from ..models import (
 from .profile_picture import profile_picture
 
 
-class MembersGrid(rx.ComponentState):
+popover_button_style = rx.Style(
+    width='2.7em',
+    height='2.7em',
+    border_radius="50%",
+    padding='0',
+    background_color='transparent',
+)
 
-    def interest_chip(interest: Interest) -> rx.Component:
-        return rx.badge(
-            interest.name,
-            color_scheme='amber',
-            radius='full',
-            variant='surface',
-        )
 
-    @classmethod
-    def get_component(cls, **props) -> rx.Component:
-        users: list[Tuple[UserAccount, City, list[Interest]]] = (
-            props.pop('users')
-        )
-        columns: int = props.pop('columns')
+def interest_chip(interest: Interest) -> rx.Component:
+    return rx.badge(
+        interest.name,
+        color_scheme='amber',
+        radius='full',
+        variant='surface',
+    )
 
-        return rx.grid(
-            rx.foreach(
-                users,
-                lambda user: rx.card(
-                    rx.box(
-                        rx.vstack(
-                            rx.hstack(
+
+def members_grid(**props) -> rx.Component:
+    users: list[Tuple[UserAccount, City, list[Interest]]] = (
+        props.pop('users')
+    )
+    columns: int = props.pop('columns')
+
+    return rx.grid(
+        rx.foreach(
+            users,
+            lambda user: rx.card(
+                rx.box(
+                    rx.vstack(
+                        rx.hstack(
+                            rx.desktop_only(
+                                rx.hover_card.root(
+                                    rx.hover_card.trigger(
+                                        rx.link(
+                                            profile_picture(
+                                                style=rx.Style(
+                                                    width='2.7em',
+                                                    height='2.7em',
+                                                ),
+                                                profile_picture=user[0].profile_picture,  # noqa: E501
+                                            ),
+                                        ),
+                                    ),
+                                    rx.hover_card.content(
+                                        user_hover_card(
+                                            user[0],
+                                            user[1],
+                                        ),
+                                    ),
+                                ),
+                            ),
+                            rx.mobile_and_tablet(
+                                rx.popover.root(
+                                    rx.popover.trigger(
+                                        rx.button(
+                                            profile_picture(
+                                                style=rx.Style(
+                                                    width='2.7em',
+                                                    height='2.7em',
+                                                ),
+                                                profile_picture=user[0].profile_picture,  # noqa: E501
+                                            ),
+                                            style=popover_button_style,
+                                        ),
+                                    ),
+                                    rx.popover.content(
+                                        user_hover_card(
+                                            user[0],
+                                            user[1],
+                                        ),
+                                    ),
+                                ),
+                            ),
+                            rx.vstack(
                                 rx.desktop_only(
                                     rx.hover_card.root(
                                         rx.hover_card.trigger(
                                             rx.link(
-                                                profile_picture(
-                                                    style=rx.Style(
-                                                        width='2.7em',
-                                                        height='2.7em',
+                                                rx.hstack(
+                                                    rx.text(
+                                                        f"{user[0].first_name}",  # noqa: E501
+                                                        class_name='desktop-medium-text',  # noqa: E501
                                                     ),
-                                                    profile_picture=user[0].profile_picture,  # noqa: E501
+                                                    rx.text(
+                                                        f"{user[0].last_name}",
+                                                        class_name='desktop-medium-text',  # noqa: E501
+                                                    ),
+                                                    spacing='1',
                                                 ),
+                                                color='inherit',
                                             ),
                                         ),
                                         rx.hover_card.content(
@@ -58,19 +114,19 @@ class MembersGrid(rx.ComponentState):
                                 rx.mobile_and_tablet(
                                     rx.popover.root(
                                         rx.popover.trigger(
-                                            rx.button(
-                                                profile_picture(
-                                                    style=rx.Style(
-                                                        width='2.7em',
-                                                        height='2.7em',
+                                            rx.link(
+                                                rx.hstack(
+                                                    rx.text(
+                                                        f"{user[0].first_name}",  # noqa: E501
+                                                        class_name='desktop-medium-text',  # noqa: E501
                                                     ),
-                                                    profile_picture=user[0].profile_picture,  # noqa: E501
+                                                    rx.text(
+                                                        f"{user[0].last_name}",
+                                                        class_name='desktop-medium-text',  # noqa: E501
+                                                    ),
+                                                    spacing='1',
                                                 ),
-                                                width='2.7em',
-                                                height='2.7em',
-                                                border_radius="50%",
-                                                padding='0',
-                                                background_color='transparent',
+                                                color='inherit',
                                             ),
                                         ),
                                         rx.popover.content(
@@ -81,73 +137,53 @@ class MembersGrid(rx.ComponentState):
                                         ),
                                     ),
                                 ),
-                                rx.vstack(
-                                    rx.hstack(
-                                        rx.text(
-                                            f"{user[0].first_name}",
-                                            class_name='desktop-medium-text',
-                                        ),
-                                        rx.text(
-                                            f"{user[0].last_name}",
-                                            class_name='desktop-medium-text',
-                                        ),
-                                        spacing='1',
-                                    ),
-                                    rx.text(
-                                        f"{user[1].name} "
-                                        f"({user[1].postal_code})",
-                                        class_name='discreet-text',
-                                    ),
-                                    spacing='1',
-                                ),
-                                width='100%',
-                                align='start',
-                            ),
-                            rx.hstack(
-                                rx.foreach(
-                                    user[2],
-                                    MembersGrid.interest_chip,
+                                rx.text(
+                                    f"{user[1].name} "
+                                    f"({user[1].postal_code})",
+                                    class_name='discreet-text',
                                 ),
                                 spacing='1',
                             ),
-                            rx.text(
-                                f"{user[0].email}",
-                                class_name='mobile-text',
-                                color_scheme='gray',
+                            width='100%',
+                            align='start',
+                        ),
+                        rx.hstack(
+                            rx.foreach(
+                                user[2],
+                                interest_chip,
                             ),
-                            rx.box(
-                                rx.cond(
-                                    ~user[0].profile_text,
-                                    rx.text(
-                                        "Aucune description",
-                                        class_name='mobile-text',
-                                        color_scheme='gray',
-                                        style={
-                                            'font-style': 'italic',
-                                        },
-                                    ),
-                                    rx.text(
-                                        f"{user[0].profile_text}",
-                                        class_name='mobile-text',
-                                        style={
-                                            'line-height': '1.4',
-                                            'letter-spacing': '0.2px',
-                                        },
-                                    ),
+                            spacing='1',
+                        ),
+                        rx.box(
+                            rx.cond(
+                                ~user[0].profile_text,
+                                rx.text(
+                                    "Aucune description",
+                                    class_name='mobile-text',
+                                    color_scheme='gray',
+                                    style={
+                                        'font-style': 'italic',
+                                    },
                                 ),
-                                margin='0.5em 0 0 0'
+                                rx.text(
+                                    f"{user[0].profile_text}",
+                                    class_name='mobile-text',
+                                    style={
+                                        'line-height': '1.4',
+                                        'letter-spacing': '0.2px',
+                                    },
+                                ),
                             ),
+                            margin='0.5em 0 0 0'
                         ),
                     ),
-                    as_child=True,
-                    size='3',
                 ),
+                as_child=True,
+                size='3',
             ),
-            columns=columns,
-            width='100%',
-            spacing='3',
-            flex_wrap='wrap',
-        )
-
-
-members_grid = MembersGrid.create
+        ),
+        columns=columns,
+        width='100%',
+        spacing='3',
+        flex_wrap='wrap',
+    )
